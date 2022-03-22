51c86861aad495f8cff54d102624a1e4559be813
klazovic1 <klazovic1@etf.unsa.ba>
2022-03-22 23:56:30 +0100

    Added crud operations for product images

---
+++ src/main/java/com/unsa/etf/InventoryAndCatalogService/controllers/ProductImagesController.java
@@ -0,0 +1,45 @@
+package com.unsa.etf.InventoryAndCatalogService.controllers;
+
+import com.unsa.etf.InventoryAndCatalogService.model.ProductImages;
+import com.unsa.etf.InventoryAndCatalogService.services.ProductImagesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productImages")
+public class ProductImagesController {
+    private final ProductImagesService productImagesService;
+
+    @Autowired
+    public ProductImagesController(ProductImagesService productImagesService) {
+        this.productImagesService = productImagesService;
+    }
+
+    @GetMapping("/get")
+    public List<ProductImages> getAllProductImages() {
+        return productImagesService.getAllProductImages();
+    }
+
+    @GetMapping("/get/{id}")
+    public ProductImages getProductImageById(@PathVariable String id) {
+        return productImagesService.getProductImageById(id);
+    }
+
+    @PostMapping("/create")
+    public void createNewProductImage(@RequestBody ProductImages productImages) {
+        productImagesService.createOrUpdateProductImage(productImages);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteProductImage(@PathVariable String id) {
+        productImagesService.deleteProductImageById(id);
+    }
+
+    @PutMapping("/update")
+    public void updateProductImage(@RequestBody ProductImages productImages) {
+        productImagesService.createOrUpdateProductImage(productImages);
+    }
+
+}
--- src/main/java/com/unsa/etf/InventoryAndCatalogService/services/ProductImagesService.java
+++ src/main/java/com/unsa/etf/InventoryAndCatalogService/services/ProductImagesService.java
@@ -1,22 +1,37 @@
 package com.unsa.etf.InventoryAndCatalogService.services;
 
+import com.unsa.etf.InventoryAndCatalogService.model.Product;
+import com.unsa.etf.InventoryAndCatalogService.model.ProductImages;
 import com.unsa.etf.InventoryAndCatalogService.repositories.ProductImagesRepository;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-//@RequiredArgsConstructor
 public class ProductImagesService {
     private final ProductImagesRepository productImagesRepository;
 
     @Autowired
-    public ProductImagesService (ProductImagesRepository productImagesRepository){
+    public ProductImagesService(ProductImagesRepository productImagesRepository) {
         this.productImagesRepository = productImagesRepository;
     }
 
+    public List<ProductImages> getAllProductImages() {
+        return productImagesRepository.findAll();
+    }
 
+    public ProductImages getProductImageById(String id) {
+        return productImagesRepository.findById(id).get();
+    }
+
+    public void deleteProductImageById(String id) {
+        productImagesRepository.deleteById(id);
+    }
+
+    public void createOrUpdateProductImage(ProductImages productImages) {
+        productImagesRepository.save(productImages);
+    }
 
     // TODO: 22.03.2022. get images
 }
