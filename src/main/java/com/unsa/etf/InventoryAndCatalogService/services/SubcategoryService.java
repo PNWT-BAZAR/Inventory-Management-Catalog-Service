66d4e4367441efe8cb81806958bd20fec7468bf9
klazovic1 <klazovic1@etf.unsa.ba>
2022-03-22 23:41:51 +0100

    Added crud operations for subcategories

--- src/main/java/com/unsa/etf/InventoryAndCatalogService/services/SubcategoryService.java
+++ src/main/java/com/unsa/etf/InventoryAndCatalogService/services/SubcategoryService.java
@@ -1,4 +1,40 @@
 package com.unsa.etf.InventoryAndCatalogService.services;
 
+import com.unsa.etf.InventoryAndCatalogService.model.Subcategory;
+import com.unsa.etf.InventoryAndCatalogService.repositories.SubcategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class SubcategoryService {
+    private final SubcategoryRepository subcategoryRepository;
+
+    @Autowired
+    public SubcategoryService(SubcategoryRepository subcategoryRepository) {
+        this.subcategoryRepository = subcategoryRepository;
+    }
+
+    public List<Subcategory> getAllSubcategories() {
+        return subcategoryRepository.findAll();
+    }
+
+    public Subcategory getSubcategoryById(String id) {
+        return subcategoryRepository.findById(id).get();
+    }
+
+    public void deleteSubcategoryById(String id) {
+        subcategoryRepository.deleteById(id);
+    }
+
+//    public void updateProduct(Product newProduct){
+//        productRepository.save(newProduct);
+//    }
+//
+//    public void createProduct(Product product){
+//        productRepository.save(product);
+//    }
+
+    public void createOrUpdateSubcategory(Subcategory newSubcategory) {
+        subcategoryRepository.save(newSubcategory);
+    }
 }
